fc0468053a8d50689c9759dfeeec9ef7517168af
ryliam <darylwilliam25@gmail.com>
2026-08-27 16:16:30 +0200

    maj railway

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    allowedHosts: 'all', // Allow any host (Railway, Netlify, etc.)
+  },
 })
 
